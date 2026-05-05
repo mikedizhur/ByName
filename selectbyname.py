2a250b1e16f20421df14617824a813c72461cf95
Mike Dizhur <mike.dizhur@gmail.com>
2026-05-05 18:34:55 +0000

    Added unhide checkmark
    No longer unhides hidden objects if they match the entered string.

--- selectbyname.py
+++ selectbyname.py
@@ -1,8 +1,8 @@
 bl_info = {
     "name": "Select by name",
-    "author": "RamenSlayer",
-    "version": (1, 0),
-    "blender": (2, 91, 2),
+    "author": "Mike Dizhur",
+    "version": (1, 1),
+    "blender": (5, 1, 1),
     "location": "View3D > Select",
     "description": "Select multiple objects with similar name with one button.",
     "category": "Object",
@@ -15,53 +15,65 @@ from bpy.types import (
     Panel,
     PropertyGroup,
 )
+from bpy.props import (StringProperty, BoolProperty)
 
-from bpy.props import (StringProperty)
 
-#function for checking if all words in a list are in a string
 def isitin(keywords, searched):
-    truthcount=0
+    """function for checking if all words in a list are in a string"""
+    truthcount = 0
     for word in keywords:
         if word.lower() in searched.lower():
-            truthcount+=1
-    if truthcount==len(keywords):
+            truthcount += 1
+    if truthcount == len(keywords):
         return True
     else:
         return False
 
 class OBJECT_OT_namedselect(Operator):
-    bl_label = "Select by name"
+    bl_label = "Select by Name"
     bl_idname = "object.named_select"
     bl_description = "Selects all object with a certain name (also unhides them)"
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
     bl_options = {'REGISTER', 'UNDO'}
-    
+
     selectnames: bpy.props.StringProperty(
-        name = "Names",
-        default = "Cube",
-        description = "String used for identifying what objects should be selected (if more than one write a space between words)"
-        )    
+        name='Names',
+        default='Cube',
+        description="Strings separated by spaces that objects names are matched against",
+        )
+
+    unhide: bpy.props.BoolProperty(
+        name="Unhide Selection",
+        default=False,
+        description="Unhide objects if the name matches",
+        )
 
     def execute(self, context):
-        select_keywords=self.selectnames.split()
+        select_keywords = self.selectnames.split()
         bpy.ops.object.select_all(action='DESELECT')
         for object in bpy.data.objects:
             if isitin(select_keywords, object.name.lower()):
+                if object.hide_get() and not self.unhide:
+                    continue
                 object.hide_set(False)
                 object.select_set(True)
         return {'FINISHED'}
 
+
 def menu_func(self, context):
     self.layout.operator(OBJECT_OT_namedselect.bl_idname)
-    
+
+
 def register():
     bpy.utils.register_class(OBJECT_OT_namedselect)
     bpy.types.VIEW3D_MT_select_object.append(menu_func)
 
+
 def unregister():
     bpy.utils.unregister_class(OBJECT_OT_namedselect)
     bpy.types.VIEW3D_MT_select_object.remove(menu_func)
-    
+
+
 if __name__ == "__main__":
     register()
